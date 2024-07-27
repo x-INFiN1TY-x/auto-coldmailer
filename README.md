f2afe7437a870e0b262ca6d41e3eb110f8bd2d11
Tanishq Ranjan <111151372+x-INFiN1TY-x@users.noreply.github.com>
2024-07-27 12:56:10 +0530

    fin commit

---
+++ README.md
@@ -0,0 +1,55 @@
+
+# Automated Coldmailer
+
+A Python script to automate sending cold emails with attachments.
+
+## Getting Started
+
+### Prerequisites
+
+* Python 3.6+
+* `pandas`, `python-dotenv`, and `openpyxl` libraries (install with `pip install pandas python-dotenv openpyxl`)
+* A CSV file with names, company names, and email IDs of people you want to cold email
+* A PDF document to attach to the email (e.g., a resume)
+* A Gmail account with a password (see instructions below to set up a password for your account)
+
+### Setup
+
+1. Clone this repository to your local machine.
+2. Create a new CSV file with the following column names: `First Name`, `Company`, `Email`. You can use Apollo.io to get emails.
+3. Create a new PDF document to attach to the email (e.g., a resume).
+4. Create a new file named `.env` in the root of the repository with the following format:
+   ```
+   SMTP_USERNAME="your email"
+   SMTP_PASSWORD="your password"
+   ```
+   Replace `your email` and `your password` with your actual Gmail account credentials.
+
+### Instructions
+
+1. Update the `email_body_template` variable in the `index.py` file to match your cold email template. You can use HTML email templates to add embedded links to your email.
+2. Update the file paths to your CSV file and PDF document in the `index.py` file (lines 11 and 48).
+3. Run the script with `python index.py`.
+
+### Troubleshooting
+
+* If you encounter any issues, you can open an issue on this repository.
+
+### Setting up a password for your Gmail account
+
+To set up a password for your Gmail account, follow these steps:
+
+1. Go to the [Google Account settings page](https://myaccount.google.com/)
+2. Click on "Security" from the left-hand menu.
+3. Scroll down to the "Signing into Google" section.
+4. Click on "App passwords".
+5. Select "Mail" as the app type.
+6. Select "Other (Custom name)" as the device type.
+7. Enter a name for the app password (e.
+8. Click on "Generate".
+9. Copy the generated password and paste it into your `.env` file.
+
+Steps to get this password for your Gmail account - [link](https://www.youtube.com/shorts/n9Ooxum-iUo)
+
+
+
---
+++ index.py
@@ -0,0 +1,134 @@
+import pandas as pd
+import smtplib
+import os
+from dotenv import load_dotenv
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from email.mime.application import MIMEApplication
+import time
+import random
+
+load_dotenv()
+
+logging.basicConfig(filename='email_log.log', level=logging.INFO)
+
+excel_file = 'your csv file.csv'
+df = pd.read_csv(excel_file)
+
+smtp_username = os.getenv('SMTP_USERNAME')
+smtp_password = os.getenv('SMTP_PASSWORD')
+server = smtplib.SMTP('smtp.gmail.com:587')
+server.ehlo()
+server.starttls()
+server.login(smtp_username, smtp_password)
+
+email_body_template = """
+Hey {name},
+<br>
+I'm Tanishq Ranjan, a 4th Year engineering undergrad at the National Institute of Technology Delhi. I came across {company} and was impressed by its remarkable growth. If feasible I'd be excited to explore potential internship opportunities within {company}. 
+<br>
+<br>
+<strong>About Me:</strong>
+<br>
+<br>
+<strong>1. Education:</strong>
+<br>
+- B.Tech Major in Electronics and Communication Engineering with a Minor in AI & ML from NIT Delhi, Graduating 2025.
+<br>
+<br>
+<strong>2. Experience:</strong>
+<br>
+- SDE Intern at Mavenir Systems Pvt Ltd
+<br>
+- Machine Learning Project Intern at DRDO - LRDE
+<br>
+- Big Data Analytics & ML Intern at DRDO - ISSA
+<br>
+- AI Project Intern at Ministry of Electronics & IT, Govt. of India
+<br>
+- Full Stack Web Development Intern at CYRAN AI Solutions, IIT Delhi
+<br>
+- LeetCode Rating: 1930+ (Knight ranked)
+<br>
+- Selected for Amazon ML School 2024
+<br>
+- Secured 98.4th Percentile out of 1.1 million candidates in JEE Mains 2021
+<br>
+<br>
+<strong>3. Technical Skills:</strong>
+<br>
+- Programming Languages: C++, Python, JavaScript
+<br>
+- Machine Learning & AI: PyTorch, TensorFlow, Neural Networks, NLP, Computer Vision
+<br>
+- Web Development: MongoDB, MySQL, Express.js, Node.js, CSS
+<br>
+- Big Data & DevOps: Apache Kafka, Apache Spark
+<br>
+- Frameworks & Libraries: Streamlit, JQuery, Scikit-Learn, Pandas, Numpy, Seaborn
+<br>
+<br>
+I would be thrilled to discuss how my background, skills, and projects align with the goals of {company}. If you're available, I would love to schedule a call to explore this opportunity further.
+<br>
+<br>
+Thank you for your time and consideration.
+<br>
+<br>
+Please find my resume attached. You can also connect with me on:
+<br>
+<a href="{linkedin_link}">LinkedIn</a> | <a href="{github_link}">GitHub</a> | <a href="{portfolio_link}">Leetcode</a>
+<br>
+<br>
+Regards,
+<br>
+<br>
+<strong>Tanishq Ranjan</strong>
+<br>
+4th Year Undergraduate Student
+<br>
+Department of Electronics and Communication Engineering
+<br>
+<strong>National Institute of Technology Delhi</strong>
+<br>
+<br>
+<strong>Email:</strong> 211220058@nitdelhi.ac.in
+<br>
+<strong>Phone:</strong> (+91) 9113963397
+<br>
+<br>
+====================================================================
+<br>
+NATIONAL INSTITUTE OF TECHNOLOGY DELHI [NITD]
+"""
+
+
+for index, row in df.iterrows():
+    time.sleep(random.randint(1, 5))
+
+    message = MIMEMultipart()
+    message['Subject'] = f"Internship Inquiry cum Application @{row['Company']} - Tanishq Ranjan, NIT Delhi"
+    message['From'] = smtp_username
+    message['To'] = row['Email']
+
+    email_body = email_body_template.format(
+        name=row['First Name'], 
+        company=row['Company'], 
+        linkedin_link="https://in.linkedin.com/in/tanishq-ranjan-26a52740",
+        github_link="https://github.com/x-INFiN1TY-x",
+        portfolio_link="https://leetcode.com/u/211220058/"
+    )
+    message.attach(MIMEText(email_body, 'html'))
+
+    pdf_path = "./path to pdf.pdf"
+    with open(pdf_path, "rb") as pdf_file:
+        pdf_attachment = MIMEApplication(pdf_file.read(), _subtype="pdf")
+        pdf_attachment.add_header('Content-Disposition', f'attachment; filename={pdf_path}')
+        message.attach(pdf_attachment)
+
+    try:
+        server.sendmail(smtp_username, row['Email'], message.as_string())
+        logging.info(f"Email sent to {row['Email']}")
+    except Exception as e:
+        logging.error(f"Error sending email to {row['Email']}: {e}")
+
+server.quit()
